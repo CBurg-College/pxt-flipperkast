d469ab5f2b1244b64ee756ef8b7ace99fdd4e2b5
CBurg-College <cburgmicro@outlook.com>
2024-10-20 18:00:06 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -12,6 +12,44 @@ namespace CBurgPinball {
         Gate2
     }
 
+    export enum LedId {
+        //% block="led 1"
+        //% block.loc.nl="led 1"
+        Led1,
+        //% block="led 2"
+        //% block.loc.nl="led 2"
+        Led2,
+        //% block="led 3"
+        //% block.loc.nl="led 3"
+        Led3
+    }
+
+    export enum ColorId {
+        //% block="dark"
+        //% block.loc.nl="donker"
+        Off,
+        //% block="red"
+        //% block.loc.nl="rood"
+        Red,
+        //% block="yellow"
+        //% block.loc.nl="geel"
+        Yellow,
+        //% block="green"
+        //% block.loc.nl="groen"
+        Green,
+        //% block="light blue"
+        //% block.loc.nl="lichtblauw"
+        Cyan,
+        //% block="blue"
+        //% block.loc.nl="blauw"
+        Blue,
+        //% block="purple"
+        //% block.loc.nl="paars"
+        Magenta,
+        //% block="white"
+        //% block.loc.nl="wit"
+        White
+    }
     export enum MediaId {
         //% block="background"
         //% block.loc.nl="achtergrond"
@@ -22,6 +60,74 @@ namespace CBurgPinball {
         //% block="video 2"
         //% block.loc.nl="video 2"
         Video2
+    }
+
+    //% block="color %ledid %state"
+    //% block.loc.nl="kleur %ledid %state"
+    export function switchled(ledid: LedId, colorid: ColorId) {
+        // do not change media
+        pins.digitalWritePin(DigitalPin.P19, 0)
+        pins.digitalWritePin(DigitalPin.P20, 0)
+        // choose the led
+        if (ledid == LedId.Led1) {
+            pins.digitalWritePin(DigitalPin.P12, 0);
+            pins.digitalWritePin(DigitalPin.P13, 1);
+        }
+        else
+        if (ledid == LedId.Led2) {
+            pins.digitalWritePin(DigitalPin.P12, 1);
+            pins.digitalWritePin(DigitalPin.P13, 0);
+        }
+        else
+        if (ledid == LedId.Led3) {
+            pins.digitalWritePin(DigitalPin.P12, 1);
+            pins.digitalWritePin(DigitalPin.P13, 1);
+        }
+        // set the color
+        if (colorid == ColorId.Off) {
+            pins.digitalWritePin(DigitalPin.P14, 0);
+            pins.digitalWritePin(DigitalPin.P15, 0);
+            pins.digitalWritePin(DigitalPin.P16, 0);
+        }
+        if (colorid == ColorId.Red) {
+            pins.digitalWritePin(DigitalPin.P14, 1);
+            pins.digitalWritePin(DigitalPin.P15, 0);
+            pins.digitalWritePin(DigitalPin.P16, 0);
+        }
+        if (colorid == ColorId.Yellow) {
+            pins.digitalWritePin(DigitalPin.P14, 1);
+            pins.digitalWritePin(DigitalPin.P15, 1);
+            pins.digitalWritePin(DigitalPin.P16, 0);
+        }
+        if (colorid == ColorId.Green) {
+            pins.digitalWritePin(DigitalPin.P14, 0);
+            pins.digitalWritePin(DigitalPin.P15, 1);
+            pins.digitalWritePin(DigitalPin.P16, 0);
+        }
+        if (colorid == ColorId.Cyan) {
+            pins.digitalWritePin(DigitalPin.P14, 0);
+            pins.digitalWritePin(DigitalPin.P15, 1);
+            pins.digitalWritePin(DigitalPin.P16, 1);
+        }
+        if (colorid == ColorId.Blue) {
+            pins.digitalWritePin(DigitalPin.P14, 0);
+            pins.digitalWritePin(DigitalPin.P15, 0);
+            pins.digitalWritePin(DigitalPin.P16, 1);
+        }
+        if (colorid == ColorId.Magenta) {
+            pins.digitalWritePin(DigitalPin.P14, 1);
+            pins.digitalWritePin(DigitalPin.P15, 0);
+            pins.digitalWritePin(DigitalPin.P16, 1);
+        }
+        if (colorid == ColorId.White) {
+            pins.digitalWritePin(DigitalPin.P14, 1);
+            pins.digitalWritePin(DigitalPin.P15, 1);
+            pins.digitalWritePin(DigitalPin.P16, 1);
+        }
+        // communicate to raspberry
+        pins.digitalWritePin(DigitalPin.P0, 1);
+        while (!pins.digitalReadPin(DigitalPin.P3));
+        pins.digitalWritePin(DigitalPin.P0, 1);
     }
 
     //% block="show %mediaid"
@@ -35,13 +141,15 @@ namespace CBurgPinball {
             pins.digitalWritePin(DigitalPin.P19, 1);
             pins.digitalWritePin(DigitalPin.P20, 1);
         }
+        else
         if (mediaid == MediaId.Video1) {
-            pins.digitalWritePin(DigitalPin.P19, 1);
-            pins.digitalWritePin(DigitalPin.P20, 0);
-        }
-        if (mediaid == MediaId.Video2) {
             pins.digitalWritePin(DigitalPin.P19, 0);
             pins.digitalWritePin(DigitalPin.P20, 1);
+        }
+        else
+        if (mediaid == MediaId.Video2) {
+            pins.digitalWritePin(DigitalPin.P19, 1);
+            pins.digitalWritePin(DigitalPin.P20, 0);
         }
         // communicate to raspberry
         pins.digitalWritePin(DigitalPin.P0, 1);
